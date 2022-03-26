22546d6f7f9fd3935db4c4387789980b07bc608e
Godson <excanny@yahoo.com>
2022-03-26 18:08:50 +0100

    ded

--- resources/views/front/welcome.blade.php
+++ resources/views/front/welcome.blade.php
@@ -491,7 +491,6 @@ Logo
 
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
   <script type="text/javascript" src="{{ asset('assets/slick/slick.min.js') }}"></script>
 
 
